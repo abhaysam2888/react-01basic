3568cccba0f7244363a1bffcfe35e09ac7b97f85
abhaysam2888 <abhaysam2888@gmail.com>
2023-10-04 18:55:26 +0530

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Chai from "./Chai"
-// uppercase the imported and exported function name
+// uppercase the imported, exported and function name
 function App() { 
+  const username = 'chai aur code'
   return (
     <>
     <Chai />
-    <h1>Hello i am abhay</h1>
+    <h1>Hello i am {username}</h1>
     <p>para</p>
     </>
   )
 }
 
+// use variable in react {}
+// <> is fragment
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return(
+    <div>
+      <h1>custom app</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const notherUser = 'chai aur react'
+
+const ReactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',
+  target :'_blank'},
+  'click me to visit google',
+  notherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <App></App>
+    ReactElement
+
 )
